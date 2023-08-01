c4338d9eb039625de9811f5632c78654bd858834
omijay <140506154+omijay@users.noreply.github.com>
2023-08-01 02:13:13 -0700

    add Transaction

--- Income and Expense Tracker/main.c
+++ Income and Expense Tracker/main.c
@@ -5,4 +5,11 @@ typedef struct {
     char description[100];
     float amount;
 } Transaction;
+void addTransaction(Transaction *transactions, int *transactionCount, char *description, float amount) {
+    Transaction newTransaction;
+    strcpy(newTransaction.description, description);
+    newTransaction.amount = amount;
 
+    transactions[*transactionCount] = newTransaction;
+    (*transactionCount)++;
+}
