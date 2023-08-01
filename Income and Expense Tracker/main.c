1b695998e6b2d34a625e4223da1319d67f160c1a
omijay <140506154+omijay@users.noreply.github.com>
2023-08-01 02:27:01 -0700

    find net savings

--- Income and Expense Tracker/main.c
+++ Income and Expense Tracker/main.c
@@ -20,3 +20,43 @@ float calculateTotal(Transaction *transactions, int transactionCount) {
     }
     return total;
 }
+int main() {
+    Transaction transactions[100];
+    int transactionCount = 0;
+
+    printf("Enter income transactions (enter '0' in description to stop):\n");
+    char description[100];
+    float amount;
+    do {
+        printf("Description: ");
+        scanf(" %[^\n]s", description);
+        if (strcmp(description, "0") == 0) {
+            break;
+        }
+        printf("Amount: ");
+        scanf("%f", &amount);
+        addTransaction(transactions, &transactionCount, description, amount);
+    } while (1);
+
+    printf("\nEnter expense transactions (enter '0' in description to stop):\n");
+    do {
+        printf("Description: ");
+        scanf(" %[^\n]s", description);
+        if (strcmp(description, "0") == 0) {
+            break;
+        }
+        printf("Amount: ");
+        scanf("%f", &amount);
+        addTransaction(transactions, &transactionCount, description, -amount);
+    } while (1);
+
+    float totalIncome = calculateTotal(transactions, transactionCount);
+    float totalExpense = calculateTotal(transactions + transactionCount / 2, transactionCount / 2);
+    printf("\nTotal Income: %.2f\n", totalIncome);
+    printf("Total Expense: %.2f\n", totalExpense);
+
+    float netSavings = totalIncome - totalExpense;
+    printf("Net Savings: %.2f\n", netSavings);
+
+    return 0;
+}
